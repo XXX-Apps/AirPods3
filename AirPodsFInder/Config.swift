ec527e15809c94764b696ed74f433849306f654e
AnnieKov <80928101+AnnieKov@users.noreply.github.com>
2025-04-06 21:23:04 +0300

    upd

--- AirPodsFInder/Config.swift
+++ AirPodsFInder/Config.swift
@@ -3,8 +3,8 @@ import UIKit
 
 enum Config {
     
-    static let terms = ""
-    static let privacy = ""
-    static let apphudKey = "app_5WQUhh1ko4e1BUjX5XEPfQSedht6Gn"
-    static let appId = "6742454046"
+    static let terms = "https://docs.google.com/document/d/1-xwhEDhMJGIJn66AHeXglyV9dMM4fNSkw6isCcHN2Kg/edit?usp=sharing"
+    static let privacy = "https://docs.google.com/document/d/1r5emOV8yLPFeKpEqiBEcRLoCKLlwsgRxL7DSoin-J2w/edit?usp=sharing"
+    static let apphudKey = "app_PZapuSTCZowDt5ZKjFen89PXrbt6iE"
+    static let appId = "6744161933"
 }
